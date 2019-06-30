76cfc865bb1e112bab461dc6db67465deb6ff6f0
charli <charlyoleg@fabfolk.com>
2019-06-30 08:16:00 +0200

    soon be able to install the pwa on desktop with chromium

--- web/ts/rouleau_app.ts
+++ web/ts/rouleau_app.ts
@@ -2,58 +2,69 @@
 
 import * as rlSub from './rouleau_sub.js';
 
+// =========================
 // dummy stuff to be deleted at the end of development
+// =========================
+
 console.log("Hello from rouleau_app.ts");
 rlSub.abc();
 
+
+// =========================
+// PWA installation
+// =========================
+
 // html elements
-const btn_pwainstall:HTMLElement = document.querySelector("#pwainstall");
-const btn_pwauninstall:HTMLElement = document.querySelector("#pwauninstall");
+const btn_pwainstall:HTMLButtonElement = document.querySelector("#pwainstall");
+const btn_pwauninstall:HTMLButtonElement = document.querySelector("#pwauninstall");
 const pwa_install_status:HTMLElement = document.querySelector(".pwa_install_status");
 
 // variable declarations
-let deferredPrompt:Event;
-
+let deferredPrompt:BeforeInstallPromptEvent;
 
 // browser triggers the following event if this app matches the PWA criteria!
-window.addEventListener('beforeinstallprompt', (evt:Event) => {
-  console.log('The browser considered the app Rouleau as a PWA');
+window.addEventListener('beforeinstallprompt', (evt:BeforeInstallPromptEvent) => {
+  console.log('INFO020: The browser considered the app Rouleau as a PWA');
+  console.log('Detected platform: ', evt.platforms);
   // Prevent Chrome 67 and earlier from automatically showing the prompt
-  //evt.preventDefault();
+  evt.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = evt;
-  // Update UI notify the user they can add to home screen
-  //btn_pwainstall.style.display = 'block';
+  // Update UI to enable the install button
+  btn_pwainstall.disabled = false;
 });
 
 // check is the pwa has been installed
-window.addEventListener('btn_appinstalled', (evt:Event) => {
+window.addEventListener('appinstalled', (evt:Event) => {
   console.log('a2hs installed');
+  pwa_install_status.innerHTML = "Rouleau is installed on your device";
+  pwa_install_status.style.background = 'green';
 });
 
 // Button to install the app
 btn_pwainstall.addEventListener('click', (evt:Event) => {
   console.log('Click on btn_pwainstall');
-  // hide our user interface that shows our A2HS button
-  //btn_pwainstall.style.display = 'none';
+  // Update UI to disable the install button again
+  btn_pwainstall.disabled = true;
   // Show the prompt
-  //deferredPrompt.prompt();
+  deferredPrompt.prompt();
   // Wait for the user to respond to the prompt
-  //deferredPrompt.userChoice
-  //  .then((choiceResult) => {
-  //    if (choiceResult.outcome === 'accepted') {
-  //      console.log('User accepted the A2HS prompt');
-  //    } else {
-  //      console.log('User dismissed the A2HS prompt');
-  //    }
-  //    //deferredPrompt = null;
-  //  });
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
   pwa_install_status.innerHTML = "Rouleau has been installed";
 });
 
 // Button to uninstall the app
 btn_pwauninstall.addEventListener('click', (evt:Event) => {
   console.log('Click on btn_pwauninstall');
+  console.log('Nothing done! Uninstall is not implemented!');
   pwa_install_status.innerHTML = "Rouleau has been uninstalled";
 });
 
