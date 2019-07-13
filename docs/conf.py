d6bc73f4791725eef8f1fcfe1886e113bc93a4fe
charli <charlyoleg@fabfolk.com>
2019-07-13 23:19:58 +0200

    try to fix issue on ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -54,3 +54,6 @@ html_theme = 'haiku'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# fix issue 'not found contents.rst' on ReadTheDocs
+master_doc = 'index'
