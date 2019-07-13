433bf138147288849012cb94a945e2369222a70b
charli <charlyoleg@fabfolk.com>
2019-07-13 22:55:34 +0200

    an alternative script to npm run the_docs

--- docs/make_the_docs.sh
+++ docs/make_the_docs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # make_the_docs.sh
 
+cd $(dirname $0)
+
+pipenv run sphinx-build -b html ./ build/
+sensible-browser build/index.html &
+
 rst2pdf rouleau_concept.rst
 evince rouleau_concept.pdf &
 
